257f12a3d6fbf06c92dab43f0215197095f13c4f
Joshua <horheyinc8@gmail.com>
2023-07-06 22:51:34 +0200

    _redirects

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let API_URL = process.env.REACT_APP_SERVER_URL;
+let API_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5005/api";
 let TOKEN = process.env.REACT_APP_TOKEN;
 //named export
 export { TOKEN };
